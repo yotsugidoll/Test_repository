d48257606f89be55b1c461e20273893f09c37e49
silly_doll <inthesky24@yandex.ru>
2023-03-28 21:01:48 +1000

    just test, nothing more

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from random import randint as ri
+
+def blah(x):
+    answer = ri(1, 100)
+    if answer == 42:
+        import this
+    elif answer == 34:
+        return 'Jail is over there onii~chan'
+    else:
+        return x
+    
